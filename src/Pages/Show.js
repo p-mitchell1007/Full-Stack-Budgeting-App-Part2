957cbe947f884f8ce13601fe3b5bf6d8c2968831
Your Name <you@example.com>
2023-07-17 20:17:10 -0400

    added a little css

--- src/Pages/Show.js
+++ src/Pages/Show.js
@@ -1,9 +1,11 @@
+import React from "react";
 import TransactionDetails from "../Components/TransactionDetails";
+import "./Show.css";
 
 function Show() {
   return (
     <div className="Show">
-      <h2>Show Transaction</h2>
+      <h2 className="show-heading">Show Transaction</h2>
       <TransactionDetails />
     </div>
   );
