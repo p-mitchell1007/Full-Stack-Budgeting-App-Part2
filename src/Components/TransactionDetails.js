80f9c00dd3905cb0c442bcf145aaacc9ace83b03
Your Name <you@example.com>
2023-07-17 20:18:34 -0400

    deleted some css

--- src/Components/TransactionDetails.js
+++ src/Components/TransactionDetails.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { Link, useParams } from "react-router-dom";
-import "./TransactionDetails.css"
+
 
 function TransactionDetails() {
   const [transaction, setTransaction] = useState({});
